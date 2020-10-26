e83875d69a4cfeaf9f344f23dd8839b7783b7e07
Sanam Sritam Jena <sanamsritam@gmail.com>
2020-10-25 20:40:35 -0400

    Completed homework 7 & quiz 7

--- Assignments/HW07_Sanam_Jena.py
+++ Assignments/HW07_Sanam_Jena.py
@@ -6,3 +6,101 @@ Objective: The goal of this assignment is to practice using list, tuple, dict, d
 There are three parts to this assignment. None of the individual functions require much code,
 but writing concise, elegant solutions may take a little thought.
 """
+
+from typing import List, DefaultDict, Tuple
+from collections import Counter, defaultdict
+import string
+
+
+class HomeWork07:
+    """In this class we will writing all the functions required to complete HomeWork07
+    """
+
+    def __init__(self) -> None:
+        return None
+
+    def anagrams_lst(self, str1: str, str2: str) -> bool:
+        """In this function we will be testing if both the parameters str1 & str2 are anagrams.
+        this function returns true if both the strings are anagrams else returns false.
+
+        Args:
+            str1 (str)
+            str2 (str)
+
+        Returns:
+            bool
+        """
+        if not isinstance(str1, str):  # If the input is not of type 'string', raise ValueError
+            raise ValueError("Input str1 must be of type str")
+        if not isinstance(str2, str):  # If the input is not of type 'string', raise ValueError
+            raise ValueError("Input str2 must be of type str")
+        return sorted(list(str1.lower())) == sorted(list(str2.lower()))
+
+    def anagrams_dd(self, str1: str, str2: str) -> bool:
+        """In this function, we will determine if two strings are anagrams using defaultdict
+
+        Args:
+            str1 (str)
+            str2 (str)
+
+        Returns:
+            bool: returns true if both str1 and str2 are anagrams
+        """
+        if not isinstance(str1, str):  # If the input is not of type 'string', raise ValueError
+            raise ValueError("Input str1 must be of type str")
+        if not isinstance(str2, str):  # If the input is not of type 'string', raise ValueError
+            raise ValueError("Input str2 must be of type str")
+        dd: defaultdict[str, int] = defaultdict(int)
+        for i in str1.lower():
+            dd[i] += 1
+        for i in str2.lower():
+            dd[i] -= 1
+        return not any(dd.values())
+
+    def anagrams_cntr(self, str1: str, str2: str) -> bool:
+        """In this function, we will determine if two strings are anagrams using only collections.Counters
+
+        Args:
+            str1 (str)
+            str2 (str)
+
+        Returns:
+            bool: returns true if both str1 and str2 are anagrams
+        """
+        if not isinstance(str1, str):  # If the input is not of type 'string', raise ValueError
+            raise ValueError("Input str1 must be of type str")
+        if not isinstance(str2, str):  # If the input is not of type 'string', raise ValueError
+            raise ValueError("Input str2 must be of type str")
+        return Counter(str1.lower()) == Counter(str2.lower())
+
+    def covers_alphabet(self, sentence: str) -> bool:
+        """This function returns True if sentence includes at least one instance of every character in the 
+        alphabet or False using only Python sets. Reffered to
+        https://stackoverflow.com/questions/59726206/how-are-these-sets-equal-eli5 and discussed with Rajat Verma.
+
+        Args:
+            sentence (str)
+
+        Returns:
+            bool
+        """
+        if not isinstance(sentence, str):  # If the input is not of type 'string', raise ValueError
+            raise ValueError("Input sentence must be of type str")
+        return set(string.ascii_lowercase) <= set(sentence.lower())
+
+    def web_analyzer(self, weblogs: List[Tuple[str, str]]) -> List[Tuple[str, List[str]]]:
+        """This function creates a summary of the weblogs with each distinct site and a sorted list of names of
+        distinct people who visited that site. Discussed with Prof Raz & Rajat Verma.
+
+        Args:
+            weblogs (List[Tuple[str, str]])
+
+        Returns:
+            List[Tuple[str, List[str]]]: each item in the list is a tuple with the employee name and the website he/she visited.
+        """
+        if not isinstance(weblogs, List):  # If the input is not of type 'List', raise ValueError
+            raise ValueError("Input weblogs must be of type List")
+        dd: DefaultDict[str, set[str]] = defaultdict(set)
+        for names, webpage in weblogs:
+            dd[webpage].add(names)
+        return [(website, sorted(name)) for website, name in sorted(dd.items())]
--- Assignments/HW07_Test_Sanam_Jena.py
+++ Assignments/HW07_Test_Sanam_Jena.py
@@ -4,3 +4,89 @@ CWID:10454295
 Date: 21 October 2020
 Objective: To write the test class in order to test HW07
 """
+from typing import List, Tuple
+import unittest
+from HW07_Sanam_Jena import HomeWork07
+
+
+class HomeWork07Test(unittest.TestCase):
+    def test_anagrams_list(self) -> None:
+        """In this function, we will be testing the anagrams_lst function from HomeWork07 class.
+        """
+        test: HomeWork07 = HomeWork07()
+        self.assertEquals(test.anagrams_lst("Dirtyroom", "Dormitory"), True)
+        self.assertEquals(test.anagrams_lst("Cinema", "Iceman"), True)
+        self.assertEquals(test.anagrams_lst("Sanam", "Manas"), True)
+        self.assertEquals(test.anagrams_lst("Sanam", "Jena"), False)
+        self.assertEquals(test.anagrams_lst("", ""), True)
+        self.assertEquals(test.anagrams_lst("123", "321"), True)
+        with self.assertRaises(ValueError, msg="Input str1 must be of type str"):
+            test.anagrams_lst(9, "Dormitory")
+        with self.assertRaises(ValueError, msg="Input str2 must be of type str"):
+            test.anagrams_lst("Dormitory", 9)
+
+    def test_anagrams_defaultdict(self) -> None:
+        """In this function, we will be testing the anagrams_dd function from HomeWork07 class.
+        """
+        test: HomeWork07 = HomeWork07()
+        self.assertEquals(test.anagrams_dd("Dirtyroom", "Dormitory"), True)
+        self.assertEquals(test.anagrams_dd("Cinema", "Iceman"), True)
+        self.assertEquals(test.anagrams_dd("Sanam", "Manas"), True)
+        self.assertEquals(test.anagrams_dd("Sanam", "Jena"), False)
+        self.assertEquals(test.anagrams_dd("", ""), True)
+        self.assertEquals(test.anagrams_dd("123", "321"), True)
+        with self.assertRaises(ValueError, msg="Input str1 must be of type str"):
+            test.anagrams_dd(9, "Dormitory")
+        with self.assertRaises(ValueError, msg="Input str2 must be of type str"):
+            test.anagrams_dd("Dormitory", 9)
+
+    def test_anagrams_cntr(self) -> None:
+        """In this function, we will be testing the anagrams_cntr function from HomeWork07 class.
+        """
+        test: HomeWork07 = HomeWork07()
+        self.assertEquals(test.anagrams_cntr("Dirtyroom", "Dormitory"), True)
+        self.assertEquals(test.anagrams_cntr("Cinema", "Iceman"), True)
+        self.assertEquals(test.anagrams_cntr("Sanam", "Manas"), True)
+        self.assertEquals(test.anagrams_cntr("Sanam", "Jena"), False)
+        self.assertEquals(test.anagrams_cntr("", ""), True)
+        self.assertEquals(test.anagrams_cntr("123", "321"), True)
+        with self.assertRaises(ValueError, msg="Input str1 must be of type str"):
+            test.anagrams_cntr(9, "Dormitory")
+        with self.assertRaises(ValueError, msg="Input str2 must be of type str"):
+            test.anagrams_cntr("Dormitory", 9)
+
+    def test_covers_alphabet(self) -> None:
+        """In this function, we will be testing the covers_alphabet function from HomeWork07 class.
+        """
+        test: HomeWork07 = HomeWork07()
+        self.assertEquals(test.covers_alphabet(
+            "qwertyuiopasdfghjklzxcvbnm"), True)
+        self.assertEquals(test.covers_alphabet(
+            "We promptly judged antique ivory buckles for the next prize"), True)
+        self.assertEquals(test.covers_alphabet("xyz"), False)
+        with self.assertRaises(ValueError, msg="Input sentence must be of type str"):
+            test.covers_alphabet(9)
+
+    def test_web_analyzer(self) -> None:
+        """In this function, we will be testing the web_analyzer function from HomeWork07 class.
+        """
+        test: HomeWork07 = HomeWork07()
+        weblogs: List[Tuple[str, str]] = [
+            ('Nanda', 'google.com'), ('Maha', 'google.com'),
+            ('Fei', 'python.org'), ('Maha', 'google.com'),
+            ('Fei', 'python.org'), ('Nanda', 'python.org'),
+            ('Fei', 'dzone.com'), ('Nanda', 'google.com'),
+            ('Maha', 'google.com'), ]
+
+        summary: List[Tuple[str, List[str]]] = [
+            ('dzone.com', ['Fei']),
+            ('google.com', ['Maha', 'Nanda']),
+            ('python.org', ['Fei', 'Nanda']), ]
+
+        self.assertEqual(test.web_analyzer(weblogs), summary)
+        with self.assertRaises(ValueError, msg="Input weblogs must be of type List"):
+            test.web_analyzer(9)
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=2)
