6c9f2ca5603bc76260312cd6e6a6c11993030b3d
Sanam Sritam Jena <sanamsritam@gmail.com>
2020-11-22 23:26:28 -0500

    Updated code for AutoPEP8

--- Assignments/AutoRunPEP8.bat
+++ Assignments/AutoRunPEP8.bat
@@ -1,3 +1,10 @@
 @echo off
-
+color 2
+echo #####Directory wide AutoPEP8 BY Sanam Jena#####
+echo Enforcing PEP8 in directory:
+echo %CD%
 for %%A IN (*.py) do start /b /wait "" autopep8 --in-place --aggressive --aggressive "%%~fA"
+echo PEP8 Successfully Enforced
+echo Press any key to exit . . .
+pause >nul
+exit
